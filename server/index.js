1b34613d3281daf3e1470b245a853143d1d8be1f
jaybau <jafbautista@gmail.com>
2019-07-17 12:56:46 -0700

    Display listing based on listing id, incorporate navigation bar

--- server/index.js
+++ server/index.js
@@ -9,21 +9,21 @@ const serverBookings = 'http://localhost:3000',
       serverRelatedListings = 'http://localhost:3001',
       serverGallery = 'http://localhost:1128';
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-app.use(express.static(path.resolve(__dirname, '..', 'client', 'dist')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/:id', express.static(path.resolve(__dirname, '..', 'client', 'dist')));
 
-app.all('/rooms/bookings/*', (req, res) => {
+app.all('/rooms/bookings/listings/:id', (req, res) => {
   console.log('redirecting to Bookings server');
   apiProxy.web(req, res, {target: serverBookings});
 });
 
-app.all('/rooms/related-listings/', (req, res) => {
+app.all('/rooms/related-listings/:id', (req, res) => {
   console.log('redirecting to Related Listings server');
   apiProxy.web(req, res, {target: serverRelatedListings});
 });
 
-app.all('/images', (req, res) => {
+app.all('/images/:houseId', (req, res) => {
   console.log('redirecting to Gallery server');
   apiProxy.web(req, res, {target: serverGallery});
 })
